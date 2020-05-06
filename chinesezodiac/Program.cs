5ffeed029931211844be272a6b30df17432dfbc2
hwon22 <s2018s06@e-mirim.hs.kr>
2020-05-06 15:25:41 +0900

    chinesezodiac 실습 소스

--- chinesezodiac/Program.cs
+++ chinesezodiac/Program.cs
@@ -10,6 +10,48 @@ namespace chinesezodiac
     {
         static void Main(string[] args)
         {
+            Console.Write("태어난 년도를 입력: ");
+            var input = int.Parse(Console.ReadLine());
+
+            switch (input%12)
+            {
+                case 0:
+                    Console.WriteLine("원숭이띠");
+                    break;
+                case 1:
+                    Console.WriteLine("닭띠");
+                    break;
+                case 2:
+                    Console.WriteLine("개띠");
+                    break;
+                case 3:
+                    Console.WriteLine("돼지띠");
+                    break;
+                case 4:
+                    Console.WriteLine("쥐띠");
+                    break;
+                case 5:
+                    Console.WriteLine("소띠");
+                    break;
+                case 6:
+                    Console.WriteLine("호랑이띠");
+                    break;
+                case 7:
+                    Console.WriteLine("토끼띠");
+                    break;
+                case 8:
+                    Console.WriteLine("용띠");
+                    break;
+                case 9:
+                    Console.WriteLine("뱀띠");
+                    break;
+                case 10:
+                    Console.WriteLine("말띠");
+                    break;
+                case 11:
+                    Console.WriteLine("양띠");
+                    break;
+            }
         }
     }
 }
